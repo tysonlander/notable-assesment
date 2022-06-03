6b3972db46e0bd993e435088415a89e0676736d5
Tyson Lander <tlander@insiderealestate.com>
2022-06-02 21:43:49 -0600

    removed static data

--- _data/doctors.js
+++
@@ -1,7 +0,0 @@
-const doctors = [
-    { "id": "ef8f0b07-8ae3-49c2-89ac-60232a92c6cd", "first_name": "Julius", "last_name": "Hibbert" },
-    { "id": "79f7168c-5564-4e64-ba72-2b409cd27fdd", "first_name": "Algernop", "last_name": "Krieger" },
-    { "id": "1cef5f5a-3780-447a-9030-bad1e261ee77", "first_name": "Nick", "last_name": "Riviera" },
-  ]
-  
-  module.exports = doctors
